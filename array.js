f7544688a71cbcc9610b90989d1dea4e5f33c207
AbuRasel-bubt <20245203027@cse.bubt.edu.bd>
2024-07-31 15:48:44 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+const products = [
+    { name: 'phone', price: 12000 }, { name: 'laptop', price: 32000 }
+]
+
+// console.log(products);
+
+const product = {
+    '0': 15,
+    '1': 60,
+    '2': 45,
+}
+
+console.log(product);
